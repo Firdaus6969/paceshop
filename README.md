f4beb42608d3c1e14935f0ccd4ee35a74791df48
Firdaus6969 <117880399+Firdaus6969@users.noreply.github.com>
2024-02-03 12:17:51 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # paceshop
+online shop website
---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>Kios Jersey | Muhamad firdaus</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+  <div class="header">
+    <p>Kios Jersey | Muhamad firdaus </p>
+  </div>
+  <nav>
+    <ul> <?php include("page/navbar.php") ?> </ul>
+  </nav>
+
+  <?php include("content.php") ?>
+
+  <?php include("page/user/keranjang.php") ?>
+  <div class="footer">
+    <p>copyright by | Muhamad firdaus | Repost by <a href="" target="_blank">Framework Id</a></p>
+  </div>
+
+</body>
+
+</html>
